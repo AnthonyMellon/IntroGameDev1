786530c69ff98dafb703d4efd922774a57e88a55
Anthony <anthonymellon@yahoo.com>
2023-02-24 15:12:24 +1300

    sheep

---
+++ Assets/RW/Scripts/Sheep.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheep : MonoBehaviour
+{
+    [SerializeField] private float runSpeed;
+    [SerializeField] private float gotHayDestroyDelay;
+    private bool hitByHay;
+    private bool dropped;
+    [SerializeField] private float dropDestroyDelay;
+    private Collider myCollider;
+    private Rigidbody myRigidbody;
+
+    private void Start()
+    {
+        myCollider = GetComponent<Collider>();
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * runSpeed * Time.deltaTime);
+    }
+
+    private void HitByHay()
+    {
+        hitByHay = true;
+        runSpeed = 0;
+
+        Destroy(gameObject, gotHayDestroyDelay);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Hay") && !hitByHay)
+        {
+            Destroy(other.gameObject);
+            HitByHay();
+        }
+        else if (other.CompareTag("DropSheep") && !dropped)
+        {
+            Drop();
+        }
+    }
+
+    private void Drop()
+    {
+        dropped = true;
+        myRigidbody.isKinematic = false;
+        myCollider.isTrigger = false;
+        Destroy(gameObject, dropDestroyDelay);
+    }
+}
